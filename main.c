502f25e07880e0f685b3108d6bfc34471f20aec2
chimny04 <lucia.roback@gmail.com>
2023-11-09 15:37:07 +0900

    10-4

--- main.c
+++ main.c
@@ -4,12 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-     char src[] = "The worst things to eat before you sleep";
-     char dst[100];
+     char str[30] = "happy C programming";
      
-     strcpy(dst, src);
-     
-     printf("copied string: %s", dst);
+     printf("length of string \"%s\": %i\n", str, strlen(str));
      
      system("PAUSE");	
      return 0;
